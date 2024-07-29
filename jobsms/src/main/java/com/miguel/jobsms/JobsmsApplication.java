63b50fa8b4e333d5fc857162261c8f654f6cf627
miguel <ninosilvagpt@gmail.com>
2024-07-29 18:00:35 -0500

    :tada: Inicialización del primer micro servicio

---
+++ jobsms/src/main/java/com/miguel/jobsms/JobsmsApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.jobsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsmsApplication.class, args);
+	}
+
+}
